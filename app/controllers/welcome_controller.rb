5167f53c07990ef9a688b5ff8058f8a77092a45d
Jesús Federico <jefp@amazon.com>
2017-07-21 13:16:49 -0400

    Added burn file to test autoscaling group

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
   end
+  def burn 
+    @t=rand(10000000)
+    @t.times do |d|
+      rand(100)*rand(100)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get "burnCPU", to: "welcome#burn"
   root 'welcome#index'
 end
